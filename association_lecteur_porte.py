4995c6ac8dcd5173793ac171fe5a8a2451985f04
jix <josagoro2000@gmail.com>
2024-04-22 16:31:44 +0200

    association lecteur portes !!!

---
+++ association_lecteur_porte.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import Set, Tuple, Iterable, Dict
+from utils.IPorte import IPorte
+from utils.ILecteur import ILecteur
+
+
+class Lecteur:
+    def __init__(self, portes: Iterable[IPorte]):
+        self.portes = portes
+
+
+class AssociationsLecteurPorte:
+    _associations: Set[Tuple[ILecteur, IPorte]] = set()
+
+    @property
+    def lecteurs_ayant_detecte_un_badge(self) -> Iterable[Lecteur]:
+        groupes: Dict[ILecteur, Iterable[IPorte]] = self._group_associations()
+        for lecteur, portes in groupes.items():
+            badge_detecte = lecteur.badge_detecte()
+            if badge_detecte is not None:
+                continue
+            yield Lecteur(portes)
+
+    def enregistrer(self, lecteur: ILecteur, porte: IPorte):
+        self._associations.add((lecteur, porte))
+
+    def _group_associations(self) -> Dict[ILecteur, Iterable[IPorte]]:
+        groupes = defaultdict(list)
+        for lecteur, porte in self._associations:
+            groupes[lecteur].append(porte)
+        return groupes
